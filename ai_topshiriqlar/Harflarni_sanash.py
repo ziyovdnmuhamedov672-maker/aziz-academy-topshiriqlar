28f69874c42d46cfbb7537b268a7e10848fd6fbc
ziyovdnmuhamedov672-maker <ziyovdnmuhamedov672@gmail.com>
2026-09-17 17:45:17 +0500

    Add AI task: Harflarni sanash

---
+++ ai_topshiriqlar/Harflarni_sanash.py
@@ -0,0 +1,24 @@
+# Harflarni sanash
+# Kurs: Dasturlash / IT
+# Mavzu: String metodlari — 1: lower, upper, strip, title
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+s = input().strip()
+
+hisob = {}
+
+for harf in s:
+    hisob[harf] = hisob.get(harf, 0) + 1 
+    
+eng_kop = max(hisob.values())
+
+harflar = []
+for harf in hisob:
+    if hisob[harf] == eng_kop:
+        harflar.append(harf)
+        
+harf = sorted(harflar)[0]
+
+print(harf)
+print(eng_kop)
